f387e49314c05d7fb3e13c824f3ecbec12ab86c3
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-09-25 03:01:30 -0300

    Player and Match Services

--- src/main/java/br/com/devth/appJogador/services/MatchService.java
+++ src/main/java/br/com/devth/appJogador/services/MatchService.java
@@ -1,4 +1,44 @@
 package br.com.devth.appJogador.services;
 
+import br.com.devth.appJogador.entities.Match;
+import br.com.devth.appJogador.repositories.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MatchService {
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    public List<Match> findAll(){
+        return matchRepository.findAll();
+    }
+
+    public Optional<Match> findById(Long id){
+        return matchRepository.findById(id);
+    }
+
+    public Match saveMatch(Match match){
+        return matchRepository.save(match);
+    }
+
+    public void deleteMatch(Long id) {
+        matchRepository.deleteById(id);;
+    }
+
+    public Match updateMatch(Long id, Match match) {
+        Match entity = matchRepository.getReferenceById(id);
+        updateData(entity, match);
+        return matchRepository.save(entity);
+    }
+
+    private void updateData(Match entity, Match match) {
+        entity.setLocal(match.getLocal());
+        entity.setDateMatch(match.getDateMatch());
+        entity.setTypeMatch(match.getTypeMatch());
+    }
 }
--- src/main/java/br/com/devth/appJogador/services/PlayerService.java
+++ src/main/java/br/com/devth/appJogador/services/PlayerService.java
@@ -1,4 +1,42 @@
 package br.com.devth.appJogador.services;
 
+import br.com.devth.appJogador.entities.Player;
+import br.com.devth.appJogador.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PlayerService {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+    public List<Player> findAll(){
+        return playerRepository.findAll();
+    }
+    public Optional<Player> findById(Long id){
+        return playerRepository.findById(id);
+    }
+
+    public Player savePlayer(Player match){
+        return playerRepository.save(match);
+    }
+
+    public void deletePlayer(Long id) {
+        playerRepository.deleteById(id);;
+    }
+
+    public Player updatePlayer(Long id, Player match) {
+        Player entity = playerRepository.getReferenceById(id);
+        updateData(entity, match);
+        return playerRepository.save(entity);
+    }
+
+    private void updateData(Player entity, Player match) {
+        entity.setName(match.getName());
+        entity.setPosition(match.getPosition());
+        entity.setRating(match.getRating());
+    }
 }
