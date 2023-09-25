b0d44a5853abb6a62221f65297fd100f6fc91651
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-09-25 03:00:30 -0300

    configuration

--- src/main/java/br/com/devth/appJogador/config/SecurityConfig.java
+++ src/main/java/br/com/devth/appJogador/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package br.com.devth.appJogador.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@Component
 public class SecurityConfig {
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
 }
