fbf2c8019c580322a3e018d2f32f9bd5bd00bd75
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-09-24 23:58:09 -0300

    Entities Match and Player

--- src/main/java/br/com/devth/appJogador/entities/Match.java
+++ src/main/java/br/com/devth/appJogador/entities/Match.java
@@ -1,4 +1,70 @@
 package br.com.devth.appJogador.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
 public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date dateMatch;
+    private String local;
+    private String typeMatch;
+    @ManyToMany
+    @Column(name = "player_id")
+    private Player player;
+
+    public Match(){
+    }
+
+    public Match(Long id, Date dateMatch, String local, String typeMatch) {
+        this.id = id;
+        this.dateMatch = dateMatch;
+        this.local = local;
+        this.typeMatch = typeMatch;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDateMatch() {
+        return dateMatch;
+    }
+
+    public void setDateMatch(Date dateMatch) {
+        this.dateMatch = dateMatch;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getTypeMatch() {
+        return typeMatch;
+    }
+
+    public void setTypeMatch(String typeMatch) {
+        this.typeMatch = typeMatch;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" +
+                "id=" + id +
+                ", dateMatch=" + dateMatch +
+                ", local='" + local + '\'' +
+                ", typeMatch='" + typeMatch + '\'' +
+                '}';
+    }
 }
--- src/main/java/br/com/devth/appJogador/entities/Player.java
+++ src/main/java/br/com/devth/appJogador/entities/Player.java
@@ -1,4 +1,79 @@
 package br.com.devth.appJogador.entities;
 
+import jakarta.persistence.*;
+
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String position;
+    private int numberMatches;
+    private double rating;
+    @ManyToMany
+    @Column(name = "match_id")
+    private Match match;
+
+    public Player(){
+    }
+
+    public Player(Long id, String name, String position, int numberMatches) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.numberMatches = numberMatches;
+        this.rating = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getNumberMatches() {
+        return numberMatches;
+    }
+
+    public void setNumberMatches(int numberMatches) {
+        this.numberMatches = numberMatches;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", numberMatches=" + numberMatches +
+                ", rating=" + rating +
+                '}';
+    }
 }
