1c4a3bb046c18f64a15050345cb9e964eb3d107a
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-09-25 03:01:09 -0300

    Match and Player Controller Updated

--- src/main/java/br/com/devth/appJogador/controllers/MatchController.java
+++ src/main/java/br/com/devth/appJogador/controllers/MatchController.java
@@ -1,4 +1,51 @@
 package br.com.devth.appJogador.controllers;
 
+import br.com.devth.appJogador.entities.Match;
+import br.com.devth.appJogador.services.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/matches")
 public class MatchController {
+
+    @Autowired
+    MatchService matchService;
+
+    @GetMapping
+    public ResponseEntity<List<Match>> findAll(){
+        List<Match> matchList = matchService.findAll();
+        return ResponseEntity.ok().body(matchList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Match>> findById(@PathVariable Long id, @RequestBody Optional<Match> match){
+        match = matchService.findById(id);
+        return ResponseEntity.ok().body(match);
+    }
+
+    @PostMapping
+    public ResponseEntity<Match> saveMatch(@RequestBody Match match){
+        match = matchService.saveMatch(match);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(match.getId()).toUri();
+        return ResponseEntity.created(uri).body(match);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Match> deleteMatch(@PathVariable Long id){
+        matchService.deleteMatch(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Match> udpdateMatch(@PathVariable Long id, @RequestBody Match match){
+        match = matchService.updateMatch(id, match);
+        return ResponseEntity.ok().body(match);
+    }
 }
--- src/main/java/br/com/devth/appJogador/controllers/PlayerController.java
+++ src/main/java/br/com/devth/appJogador/controllers/PlayerController.java
@@ -1,4 +1,51 @@
 package br.com.devth.appJogador.controllers;
 
+import br.com.devth.appJogador.entities.Player;
+import br.com.devth.appJogador.services.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/players")
 public class PlayerController {
+
+    @Autowired
+    PlayerService playerService;
+
+    @GetMapping
+    public ResponseEntity<List<Player>> findAll(){
+        List<Player> matchList = playerService.findAll();
+        return ResponseEntity.ok().body(matchList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Player>> findById(@PathVariable Long id, @RequestBody Optional<Player> player){
+        player = playerService.findById(id);
+        return ResponseEntity.ok().body(player);
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> saveMatch(@RequestBody Player player){
+        player = playerService.savePlayer(player);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(player.getId()).toUri();
+        return ResponseEntity.created(uri).body(player);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Player> deleteMatch(@PathVariable Long id){
+        playerService.deletePlayer(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Player> udpdateMatch(@PathVariable Long id, @RequestBody Player player){
+        player = playerService.updatePlayer(id, player);
+        return ResponseEntity.ok().body(player);
+    }
 }
